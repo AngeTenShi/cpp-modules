8547817a437f355cf992722cae775ecf16d0ed42
AngeTenShi <megazombie06pro@gmail.com>
2023-02-13 13:46:11 +0100

    ex02

---
+++ cpp-07/ex02/Array.hpp
@@ -0,0 +1,61 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+#include <stdexcept>
+
+template <typename T>
+
+class Array
+{
+public:
+    Array<T> (void) : elem(0), _size(0)
+    {
+        std::cout << "Constructor default called" << std::endl;
+    };
+    Array<T> (unsigned int n) : elem(new T[n]), _size(n)
+    {
+        std::cout << "Constructor n element called" << std::endl;
+    };
+    Array<T> (Array<T> const &other)
+    {
+        int i = 0;
+        this->elem = new T[other.size()];
+        while (i < other.size())
+        {
+            this->elem[i] = other.elem[i];
+        }
+        this->_size = other.size();
+    };
+    ~Array<T> (void)
+    {
+        std::cout << "Destructor called" << std::endl;
+        delete [] this->elem;
+    }
+    Array<T> &operator=(Array<T> const &other)
+    {
+        int i = 0;
+        this->elem = new T[other.size()];
+        while (i < other.size())
+        {
+            this->elem[i] = other.elem[i];
+        }
+        this->_size = other.size();
+        return (*this);
+    };
+    T   &operator[](int index)
+    {
+        if (index < 0 || index >= this->_size)
+            throw (std::range_error("Index is out of range"));
+        return(this->elem[index]);
+    }
+    int size() const {
+        return (this->_size);
+    };
+private:
+    T   *elem;
+    int _size;
+};
+
+
+#endif
---
+++ cpp-07/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include "Array.hpp"
+
+int main(void)
+{
+    Array <int> test;
+    Array <int> test2(5);
+    Array <char> test3(5);
+
+    std::cout << test2[4] << std::endl;
+    test2[4] = 26;
+    std::cout << test2[4] << std::endl;
+    test3[0] = 'a';
+    std::cout << test3[0] << std::endl;
+    std::cout << test2[5] << std::endl;
+    return (0);
+}

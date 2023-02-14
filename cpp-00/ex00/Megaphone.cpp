18c39ae6c1e0f9bba9ce70635a1bc7f6ecd18a40
Ange <anggonza@student.42nice.fr>
2023-02-14 16:57:54 +0100

    ex00

--- cpp-00/ex00/Megaphone.cpp
+++ cpp-00/ex00/Megaphone.cpp
@@ -3,29 +3,21 @@
 int	main(int ac, char **av)
 {
 	int i = 1;
-	int j = 0;
-
 	if (ac < 2)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	while (av[i])
+	while (i < ac)
 	{
-		while (av[i][j])
+
+		std::string	manip = av[i];  
+		for (std::string::size_type j = 0; j < manip.size(); j++)
 		{
-			if (i == ac - 1)
-			{
-				std::cout << (char) std::toupper(av[i][j]);
-				if (!av[i][j + 1])
-					std::cout << (char) std::toupper(av[i][j]) << std::endl;
-			}
-			else
-				std::cout << (char) std::toupper(av[i][j]);
-			j++;
+			std::cout << static_cast<char>(std::toupper(manip[j]));
 		}
-		j = 0;
 		i++;
 	}
+	std::cout << std::endl;
 	return (0);
 }

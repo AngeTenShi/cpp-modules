27d33bcf5de7b45a3d92cf319c4f0ce162779685
AngeTenShi <megazombie06pro@gmail.com>
2023-02-16 18:16:00 +0100

    ex01 CPP-08 & CPP-06 patch

---
+++ cpp-08/ex01/main.cpp
@@ -0,0 +1,21 @@
+#include "Span.hpp"
+
+int main()
+{
+    std::srand(time(NULL));
+    std::vector<int> v(10000);
+    std::generate(v.begin(), v.end(), std::rand);
+    Span sp2 = Span(10000);
+    Span sp = Span(5);
+    sp2.addRangeNumber(v.begin(), v.end());
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+    std::cout << sp2.longestSpan() << std::endl;
+    std::cout << sp2.shortestSpan() << std::endl;
+    return 0;
+}
